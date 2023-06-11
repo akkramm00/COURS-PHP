84ac212c5a0053ee4989a3f06e9218c5281cde4e
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-06-11 21:37:19 +0000

    add all

--- index.php
+++ index.php
@@ -11,24 +11,24 @@
     <div class="row">
       <div class="mb-3">
         <label for="exampleInputtext" class="form-label">Pseudo</label>
-        <input type="text" class="form-control" id="exampleInputtext" aria-describedby="textHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <input type="text" class="form-control" name="Pseudo" id="exampleInputtext" aria-describedby="textHelp">
+        <div id="emailHelp" class="form-text"></div>
       </div>
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text"></div>
       </div>
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <input type="password" class="form-control" name="password" id="exampleInputPassword1">
       </div>
       <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        <input type="checkbox" class="form-check-input" name="valider" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Accepter les conditions</label>
       </div>
            </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-primary">Valider</button>
     
 </form>
 
